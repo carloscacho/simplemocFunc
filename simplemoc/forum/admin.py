9ea5478effa0510ce3743d7ab19ac2117943e35b
carloscacho <carlosemilio.ac@gmail.com>
2018-04-06 10:47:29 -0400

    changes of design

--- simplemoc/forum/admin.py
+++ simplemoc/forum/admin.py
@@ -2,14 +2,21 @@ from django.contrib import admin
 
 from .models import Thread, Replay
 
+
+class ReplayAdmin(admin.ModelAdmin):
+    list_display=['thread', 'author', 'created','correct','replay_up',  'replay_down', 'modified']
+    fields=['thread', 'author','replay', 'correct',('replay_up',  'replay_down')]
+    search_fields=['thread__title', 'author__email', 'replay']
+    list_filter = ['thread__title', 'author__email', 'created', 'modified' ]  
+
+class ReplayInline(admin.TabularInline):
+    model = Replay
+
 class ThreadAdmin(admin.ModelAdmin):
     list_display=['title', 'author', 'created', 'modified']
     search_fields=['title', 'author__email', 'body']
     prepopulated_fields = {'slug': ('title',)}
-
-class ReplayAdmin(admin.ModelAdmin):
-    list_display=['thread', 'author', 'created','correct', 'replay_up', 'modified']
-    search_fields=['thread__title', 'author__email', 'replay']    
+    inlines = [ReplayInline]
 
 admin.site.register(Thread, ThreadAdmin)
 admin.site.register(Replay, ReplayAdmin)
